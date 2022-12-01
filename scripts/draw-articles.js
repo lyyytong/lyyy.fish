8c078fa03e3bc434f2db41abe29355773f0650b0
Ly Tong <lyyytong@gmail.com>
2022-12-01 14:02:57 +0700

    .

--- scripts/draw-articles.js
+++ scripts/draw-articles.js
@@ -1,90 +1,112 @@
-const styles = getComputedStyle(document.querySelector('body'))
-const largeLineHeight = parseFloat(styles.lineHeight.split('px')[0]) * 1.2
-const screenHeight = window.innerHeight
-
 drawList()
 async function drawList() {
     const data = await d3.csv('data/articles.csv')
     data.sort((a,b)=>d3.descending(a.date,b.date))
-    const listHeight = data.length * largeLineHeight
-    const margin = screenHeight * .1
-    const height = listHeight + margin * 2
-    let dim = {
-        width: window.innerWidth,
-        height: d3.max([height, screenHeight]),
-        margin: {
-            top: margin,
-            bottom: margin,
-        }
-    }
-    dim.margin.left = dim.width * .1
-    dim.margin.right = dim.width * .1
-    dim.boundedWidth = dim.width - dim.margin.left - dim.margin.right
-    dim.boundedHeight = dim.height - dim.margin.top - dim.margin.bottom
-    const textMargin = 10
+    
+    let styles
+    let lineHeight
+    let listHeight
+    let marginTopBot
+    let height
+    let dim
+    let coords
 
     const wrapper = d3.select('#articles').append('svg')
-        .attr('width', dim.width).attr('height', dim.height)
     const bounds = wrapper.append('g')
-        .style('transform', `translateX(
-            ${dim.margin.left}px
-        )`)
+    const textBounds = bounds.append('g')
+
+    updateDimensions()
 
     const getRandomX = d3.randomUniform(dim.boundedWidth)
     const getRandomXLeft = d3.randomUniform(dim.boundedWidth * .3)
     const getRandomXRight = d3.randomUniform(dim.boundedWidth * .7, dim.boundedWidth)
-    const randomX1 = getRandomX()
-    const randomX2 = randomX1 >= dim.boundedWidth / 2 ? getRandomXLeft() : getRandomXRight()
-    const randomWidth = Math.abs(randomX1 - randomX2)
-    const alpha = Math.atan(randomWidth / dim.height)
+    let randomX1 = getRandomX()
+    let randomX2 = randomX1 >= dim.boundedWidth / 2 ? getRandomXLeft() : getRandomXRight()
+    let randomWidth = Math.abs(randomX1 - randomX2)
+    let alpha = Math.atan(randomWidth / dim.height)
 
-    bounds.append('line').attr('class', 'article-axis')
+    const articleAxis = bounds.append('line')
+        .attr('class', 'article-axis')
         .attr('x1', randomX1).attr('x2', randomX2)
         .attr('y2', dim.height)
-
-    const textBounds = bounds.append('g')
-
+    
     const articles = textBounds.selectAll('.article')
-        .data(data)
-        .join('a').attr('class', 'article')
+        .data(data).join('a')
+        .attr('class', 'article')
         .attr('href', d => d.name != '(coming soon)' ? 'pages/' + d.name.replaceAll(' ', '-') + '.html' : '')
         .append('text')
             .html(d => d.name)
-            .attr('y', (d, i) => largeLineHeight * i + largeLineHeight / 2)
 
-    if (height < screenHeight) {
-        textBounds.style('transform', `translateY(
-            ${dim.boundedHeight / 2 - listHeight / 2 + dim.margin.top}px
-        )`)
+    updateListCoords()
+
+    window.addEventListener('resize',updateList)
+    function updateList(){
+        const oldBoundedWidth = dim.boundedWidth
+        updateDimensions()
+
+        randomX1 = randomX1 / oldBoundedWidth * dim.boundedWidth
+        randomX2 = randomX2 / oldBoundedWidth * dim.boundedWidth
+        randomWidth = Math.abs(randomX1 - randomX2)
+        alpha = Math.atan(randomWidth / dim.height)
+        
+        articleAxis.attr('x1', randomX1)
+            .attr('x2', randomX2).attr('y2', dim.height)
+        
+        updateListCoords()
     }
-    else {
-        textBounds.style('transform', `translateY(
-            ${dim.margin.top}px
+    function updateDimensions(){
+        styles = getComputedStyle(document.querySelector('body'))
+        lineHeight = parseFloat(styles.lineHeight.split('px')[0]) * 1.2
+
+        listHeight = data.length * lineHeight
+        marginTopBot = window.innerHeight * .1
+        height = listHeight + marginTopBot * 2
+
+        dim = {
+            margin: {}
+        }
+        dim.width = window.innerWidth
+        dim.height = d3.max([height, window.innerHeight])
+        dim.margin.top = marginTopBot
+        dim.margin.bottom = marginTopBot
+        dim.margin.left = window.innerWidth *.1
+        dim.margin.right = window.innerWidth *.1
+        dim.boundedWidth = dim.width - dim.margin.left - dim.margin.right
+        dim.boundedHeight = dim.height - dim.margin.top - dim.margin.bottom
+
+        wrapper.attr('width', dim.width).attr('height', dim.height)
+        bounds.style('transform', `translateX(
+            ${dim.margin.left}px
         )`)
+        textBounds.style('transform', `translateY(
+            ${height < window.innerHeight 
+                ? dim.boundedHeight / 2 - listHeight / 2 + dim.margin.top
+                : dim.margin.top
+        }px)`)
     }
-
-    let xCoords = []
-    articles.nodes().forEach((d, i) => {
-        const triangleHeight = d.getBoundingClientRect().y+d.getBoundingClientRect().height/2
-        const triangleBase = Math.tan(alpha) * triangleHeight
-        const x = randomX1 > randomX2 ? randomX1 - triangleBase : randomX1 + triangleBase
-        const textWidth = d.getBoundingClientRect().width
-        const rightX = x + textWidth + textMargin
-        const textAnchor = rightX > dim.boundedWidth ? 'end' : 'start'
-        const x0 = textAnchor == 'start' ? x - textMargin : x + textMargin
-        const delay = 1200 + 30 * i
-        xCoords.push({ x: x, x0: x0, textAnchor: textAnchor, delay: delay })
-    })
-
-    articles
-        .attr('x', (d, i) => xCoords[i].x0)
-        .style('text-anchor', (d, i) => xCoords[i].textAnchor)
-    articles.nodes().forEach((d, i) => {
-        d3.select(d).transition()
-            .duration(xCoords[i].delay)
-            .ease(d3.easeElasticOut)
-            .attr('x', xCoords[i].x)
-    })
+    function updateListCoords(){
+        articles.attr('y', (d, i) => lineHeight * i + lineHeight / 2)
+        coords = []
+        articles.nodes().forEach((d, i) => {
+            const triangleHeight = d.getBoundingClientRect().y+d.getBoundingClientRect().height/2
+            const triangleBase = Math.tan(alpha) * triangleHeight
+            const x = randomX1 > randomX2 ? randomX1 - triangleBase : randomX1 + triangleBase
+            const textWidth = d.getBoundingClientRect().width
+            const rightX = x + textWidth
+            const textAnchor = rightX > dim.boundedWidth ? 'end' : 'start'
+            const x0 = textAnchor == 'start' ? x - 10 : x + 10
+            const delay = 1000 + 30 * i
+            coords.push({ x: x, x0: x0, textAnchor: textAnchor, delay: delay })
+        })
+        articles.attr('x', (d, i) => coords[i].x0)
+            .style('text-anchor', (d, i) => coords[i].textAnchor)
+        articles.nodes().forEach((d, i) => {
+            d3.select(d).transition()
+                .duration(coords[i].delay)
+                .ease(d3.easeElasticOut)
+                .attr('x', coords[i].x)
+        })
+    }
 }
 
 const about = d3.select('#about')
